a8eea11c45aeb6825f434b3b249d7b112b180ce9
jasonsub <win7>
2014-05-15 04:23:04 -0600

    change single bit output blif generator to whole ckt generator

--- oldexperiment/BDD_blif.pl
+++ oldexperiment/BDD_blif.pl
@@ -4,9 +4,9 @@ use 5.014;
 use strict;
 use warnings;
 
-open BL, ">", "65bit_BDD.blif";
+open BL, ">", "spec_5bit.blif"; # change filename
 
-my $size = 65;
+my $size = 5; # change this size
 
 my $d = 1;
 my $c = 1;
@@ -14,25 +14,28 @@ my $b = 1;
 my $i = 0;
 # my @idx = qw /1 0 6 6 15 12 23 21 31 14 32 1 2 15 23 19 26 28 30 13 17 22 25 3 16 10 31 5 22 2 7 12 26 10 29 20 25 8 24 18 30 4 24 11 14 3 7 19 21 11 18 8 16 28 29 9 27 17 27 9 20 4 13 5 32/;
 
-my @idx = qw /1 0 7 7 46 14 31 43 53 29 63 45 64 1 2 31 46 18 56 23 27 40 62 16 44 30 55 3 47 35 43 12 41 52 55 9 32 21 27 26 50 19 33 26 37 10 57 40 60 36 49 20 22 10 19 50 62 5 54 13 45 3 8 18 53 21 51 37 42 15 48 25 41 22 34 48 57 59 61 11 24 16 36 34 52 4 15 12 63 6 30 2 8 14 56 35 38 25 61 20 28 33 54 17 42 4 32 29 51 13 17 9 4 7 23 38 59 60 39 58 24 58 39 49 11 28 5 44 6 64/;
+my @idx = qw /1 0 0 3 3 4 1 2 2 4/;
 my $j = 1;
 
-print BL ".model outputBDD\n.inputs ";
+print BL ".model specckt\n.inputs ";
 for($i=0;$i<$size;$i++){
 print BL "a$i b$i ";
 }
-print BL "\n.outputs R\n.names b0 a1 d0\n11 1\n";
-
+my $k;
+for($k = 0; $k < $size; $k++){
+printf BL "\n.outputs R\n.names b%d a%d d0\_r%d\n11 1\n", $k, $k+1, $k;
+$j = 1; $b = 1; $c = 1; $d = 1;
 while ($j < $size) {
-	printf BL (".names a%d a%d c%d\n01 1\n10 1\n", $idx[2*$j-1], $idx[2*$j], $c); $c++;
-	printf BL (".names b%d c%d d%d\n11 1\n", $b, $c-1, $d); $b++; $d++;
+	printf BL (".names a%d a%d c%d\_r%d\n01 1\n10 1\n", $idx[2*$j-1], $idx[2*$j], $c, $k); $c++;
+	printf BL (".names b%d c%d d%d\_r%d\n11 1\n", $b, $c-1, $d, $k); $b++; $d++;
 	$j ++;
 }
-
-print BL ".names d0 d1 tmp0\n01 1\n10 1\n";
+$i = 0;
+printf BL ".names d0\_r%d d1\_r%d tmp0\_r%d\n01 1\n10 1\n", $k, $k, $k;
 for($i = 2; $i < $size; $i++){
-	printf BL (".names tmp%d d%d tmp%d\n01 1\n10 1\n", $i-2, $i, $i-1);
+	printf BL (".names tmp%d\_r%d d%d\_r%d tmp%d\_r%d\n01 1\n10 1\n", $i-2,$k, $i,$k, $i-1,$k);
 }
 
-printf BL (".names tmp%d d%d R\n01 1\n10 1\n", $size-2, $size-1);
+printf BL (".names tmp%d\_r%d d%d\_r%d R%d\n01 1\n10 1\n", $size-2,$k, $size-1,$k,$k);
+}
 print BL ".end\n";
